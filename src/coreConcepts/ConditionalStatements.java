8012142e36baceb798c6d62e4e0f4e1ab4c747e4
NagKothapalli <nag022@gmail.com>
2023-03-21 07:58:47 +0530

    March21st code pushed

--- src/coreConcepts/ConditionalStatements.java
+++ src/coreConcepts/ConditionalStatements.java
@@ -22,6 +22,43 @@ public class ConditionalStatements
 		System.out.println("End of my program");
 	}
 	@Test
+	public void compareNumbers_abc()  //TDD : Test Driven Development
+	{
+		compareNumbers(44,44);
+		compareNumbers(24,44);
+	}
+	public void compareNumbers(int a,int b) //Dev code / source code
+	{
+		System.out.println("First Number :" + a);
+		System.out.println("Second Number :" + b);
+		if(a==b)  // = : assignment    , == : relational 
+		{ //true
+			System.out.println("Both A and B are equal");
+		}
+		System.out.println("End of my program");
+	}
+	@Test
+	public void compareNumbers_ifelse_test()
+	{
+		compareNumbers_ifelse(55,88); // 4/8   = 50
+		compareNumbers_ifelse(55,55); // 4/8   = 50
+	}
+	//Code Coverage = Lines covered / Total Lines * 100
+	public void compareNumbers_ifelse(int a,int b) //Dev code / source code
+	{
+		System.out.println("First Number :" + a);
+		System.out.println("Second Number :" + b);
+		if(a==b)  // = : assignment    , == : relational 
+		{ //true
+			System.out.println("Both A and B are equal");
+		}
+		else
+		{ //false
+			System.out.println("Both A and B are  equal");
+		}
+		System.out.println("End of my program");
+	}
+	@Test
 	public void printAll()
 	{
 		System.out.println("First Number :" + a);
