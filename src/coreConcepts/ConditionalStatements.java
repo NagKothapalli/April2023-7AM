474df6432b4fb84c9c9ac91a95e199ee7d58c13e
NagKothapalli <nag022@gmail.com>
2023-03-22 07:58:15 +0530

    March22nd code pushed

--- src/coreConcepts/ConditionalStatements.java
+++ src/coreConcepts/ConditionalStatements.java
@@ -58,6 +58,141 @@ public class ConditionalStatements
 		}
 		System.out.println("End of my program");
 	}
+	//WAP to read two numbers as inputs and perform addition if they are equal if not perform subtraction
+	@Test
+	public void workWithIfElse_test()
+	{
+		workWithIfElse(55,55);
+		workWithIfElse(85,55);
+		workWithIfElse(25,55);
+		workWithIfElse(-55,55);
+		workWithIfElse(85,-55);
+	}
+	
+	public void workWithIfElse(int a,int b) //Dev code / source code
+	{
+		System.out.println("First Number :" + a);
+		System.out.println("Second Number :" + b);
+		boolean flag = (a==b);
+		System.out.println("My flag value :" + flag);
+		if(flag)
+		{ //true
+			int sum = (a+b);  // int = int+int
+			System.out.println("Sum of two Numbers :" + sum);
+		}
+		else
+		{ //false
+			int diff = (a-b);
+			System.out.println("Difference of two Numbers :" + diff);
+		}
+	}
+	
+	//WAP to read two numbers as inputs and perform addition if they are equal,do subtraction if a>b and do multiplication if a<b
+	@Test
+	public void workWithElseIf_test()
+	{
+		workWithElseIf(44,44);
+		workWithElseIf(22,88);
+		workWithElseIf(66,12);
+	}
+	public void workWithElseIf(int a,int b) //Dev code / source code
+	{
+		System.out.println("First Number :" + a);
+		System.out.println("Second Number :" + b);
+		if(a==b) //true|false
+		{ //true
+			int sum = (a+b);  // int = int+int
+			System.out.println("Sum of two Numbers :" + sum);
+		}
+		else if(a>b)
+		{ //true
+			int diff = (a-b);
+			System.out.println("Difference of two Numbers :" + diff);
+		}
+		else if(a<b)
+		{
+			int prod = (a*b);
+			System.out.println("Product of two Numbers:"+prod);
+		}
+	}
+	
+	//WAP to read two numbers as inputs and perform the below task if and only if both inputs are above 10
+	 //Task : perform addition if they are equal,do subtraction if a>b and do multiplication if a<b
+	@Test
+	public void workWithNestedIfCondition_test()
+	{
+		workWithNestedIfCondition(4,8);
+		workWithNestedIfCondition(4,88);
+		workWithNestedIfCondition(44,8);
+		workWithNestedIfCondition(44,44);
+		workWithNestedIfCondition(44,88);
+		workWithNestedIfCondition(14,12);		
+	}
+	public void workWithNestedIfCondition(int a,int b)
+	{
+		System.out.println("First Number :" + a);
+		System.out.println("Second Number :" + b);
+		//    T        T   -> T
+		if((a>10) && (b>10)) // Outer if // &&(AND) : Logical Operator
+		{
+			//Task
+			if(a==b) //Nested if - inner if  // == , < , > : Relational Operators
+			{ //true
+				int sum = (a+b); // + , - , * : Arithmetic Operators  // int = int+int
+				System.out.println("Sum of two Numbers :" + sum);
+			}
+			else if(a>b)
+			{ //true
+				int diff = (a-b); // = : Assignment Operator
+				System.out.println("Difference of two Numbers :" + diff);
+			}
+			else if(a<b)
+			{
+				int prod = (a*b);
+				System.out.println("Product of two Numbers:"+prod);
+			}			
+		}
+		else
+		{
+			//not above 10
+			System.out.println("Both A and B or any one of them might be below 10");
+		}
+	}
+	@Test
+	public void workWithNestedIfCondition2_test()
+	{
+		workWithNestedIfCondition2(4,6);
+		workWithNestedIfCondition2(44,6);
+		workWithNestedIfCondition2(4,66);
+		workWithNestedIfCondition2(24,24);
+		workWithNestedIfCondition2(44,26);
+		workWithNestedIfCondition2(24,66);
+	}
+	
+	public void workWithNestedIfCondition2(int a,int b)
+	{		
+		//    T        T   -> T
+		if((a>10) && (b>10)) // Outer if // &&(AND) : Logical Operator
+		{
+			workWithElseIf(a,b);//Task			
+		}
+		else
+		{	//not above 10
+			System.out.println("First Number :" + a);
+			System.out.println("Second Number :" + b);
+			System.out.println("Both A and B or any one of them might be below 10");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@Test
 	public void printAll()
 	{
